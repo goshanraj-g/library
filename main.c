e3c88d790829603c5afc38b87f763cb97cb922f7
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-04-05 21:17:08 -0400

    create node

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct
+typedef struct Book
 {
     char *title;
     char *author;
@@ -10,20 +10,22 @@ typedef struct
     struct Book *next;
 } Book;
 
-// without typdef, we would have to use struct Book myBook;
-
-Book createBook(char *title, char *author, int year)
+Book *createBook(char *title, char *author, int year)
 {
     Book *newBook = malloc(sizeof(Book));
     newBook->title = malloc(strlen(title) + 1);
     strcpy(newBook->title, title);
     newBook->author = malloc(strlen(author) + 1);
     strcpy(newBook->author, author);
-    newBook->author
+    newBook->year = year;
+    return newBook;
 };
 
 int main(void)
 {
+    
+    Book *head = NULL;
+
 
     return 0;
 };
